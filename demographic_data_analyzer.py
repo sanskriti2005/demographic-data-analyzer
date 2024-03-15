9388863a1188adeb3b9376eb2b0d9a35085605c5
Sanskriti <sanskritisri27@gmail.com>
2024-03-15 05:42:19 +0000

    First draft code for percentage of people with bachelors

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -13,7 +13,9 @@ def calculate_demographic_data(print_data=True):
     average_age_men = male_df['age'].mean
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    total_people = len(df['education'])
+    bachelors_people = (df['education'] == 'Bachelors').sum()
+    percentage_bachelors = (bachelors_people / total_people) * 100
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
